58107d8117f82fb8cc85e96c121b4da63d94bce7
agartanah <vyatin2017@mail.ru>
2024-11-03 22:59:20 +0700

    add styles from previous lab. add form for task

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import FormTask from './components/FormTask'
+import "../src/styles/main.css"
 
 function App() {
 
--- src/components/FormTask.jsx
+++ src/components/FormTask.jsx
@@ -1,6 +1,6 @@
 export default function FormTask() {
     return (
-        <>
+        <div id="form-task-container" class="form-task-container">
             <div class="input-container">
                 <input type="text" class="input-task" placeholder="Title..."/>
                 <input type="text" class="input-task" placeholder="About..."/>
@@ -9,6 +9,6 @@ export default function FormTask() {
                 <button type="submit" id="add-button" class="add-button">
                 </button>
             </div>
-        </>
+        </div>
     );
 }
